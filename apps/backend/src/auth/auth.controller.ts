f83361f1db72c2bd690016e8a28b65a67f41b4c8
Ajay Patel <patel.ajay745@gmail.com>
2025-12-05 12:24:35 -0500

    feat: implement mobile authentication screens and backend protected route

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, UseGuards } from "@nestjs/common";
 import { CurrentUser } from "../decorators/current-user.decorator";
+import { Protected } from "../decorators/protected.decorator";
 import { ClerkAuthGuard } from "./guards/clerkGuard";
 import type { User } from "@clerk/backend";
 
 @Controller("auth")
 export class AuthController {
-    @Get('me')
+    @Get('/me')
+    @Protected()
     @UseGuards(ClerkAuthGuard)
     async getProfile(@CurrentUser() user: User) {
         return user;
---
+++ apps/mobile/app/(home)/index.tsx
@@ -0,0 +1,34 @@
+import {SignedIn, SignedOut, useAuth, useUser} from "@clerk/clerk-expo";
+import {Link, Redirect} from "expo-router";
+import {Text, View} from "react-native";
+import {SignOutButton} from "@/app/components/sign-out-button";
+
+export default function Page() {
+  const {user} = useUser();
+  const {isSignedIn} = useUser();
+  const {getToken} = useAuth();
+
+  if (!isSignedIn) return <Redirect href={"/sign-in"} />;
+
+  (async () => {
+    const token = await getToken({template: "aj"});
+    console.log("User Token:", token);
+  })();
+
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  );
+}
