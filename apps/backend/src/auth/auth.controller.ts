53d845ceb4565f784dfafc2acdc45e03cc8e2fa6
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-12-15 18:51:47 +0530

    refactor: remove unused getUserDetails route from AuthController

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,13 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {
-
-    @Protected()
-    @UseGuards(ClerkAuthGuard)
-    @Get("/")
-    getUserDetails(@CurrentUser() user: User) {
-        return user
-    }
-
-}
+export class AuthController {}
